d528903f43264677644595e94686616763e8d32a
karliem13 <karliemoney1996@gmail.com>
2026-06-28 14:55:35 -0500

    Add Science Library page with SunbeamCartographer

---
+++ src/pages/ScienceLibrary.jsx
@@ -0,0 +1,14 @@
+import { SunbeamCartographer } from '../components/SunbeamCartographer';
+import '../styles/SunbeamCartographer.css';
+
+/**
+ * Science Library Page
+ * Contains scientific tools and references for plant care
+ */
+export const ScienceLibrary = () => {
+  return (
+    <div className="science-library-page">
+      <SunbeamCartographer />
+    </div>
+  );
+};
